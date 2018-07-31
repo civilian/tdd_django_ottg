7ace66328c83be3fdb381b31b40fab142babf6e5
civilian <chamatoscar@gmail.com>
2018-07-31 13:02:44 -0500

    hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n8h)62u%car%z1=hx%6(t9z1=($&e+f(7oac*96efug7lip!f='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

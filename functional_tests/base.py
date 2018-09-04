bfd2c5064f2a9303efe798140f9a97050d6b9e90
civilian <chamatoscar@gmail.com>
2018-09-04 13:57:33 -0500

    Create Page objects for list pages, use in sharing FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -18,6 +18,19 @@ SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
 )
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -74,25 +87,6 @@ class FunctionalTest(StaticLiveServerTestCase):
             timestamp=timestamp
         )
 
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
-
-
-    @wait
-    def wait_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
 
     @wait
     def wait_for(self, fn):
@@ -124,6 +118,11 @@ class FunctionalTest(StaticLiveServerTestCase):
         item_number = num_rows + 1
         self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
 
+    @wait
+    def wait_for_row_in_list_table(self, item_text, item_numer):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def create_pre_authenticated_session(self, email):
         if self.staging_server:

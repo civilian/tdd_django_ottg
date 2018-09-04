f6abe726d87308c388152819e97850d620303eac
civilian <chamatoscar@gmail.com>
2018-09-04 11:51:39 -0500

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,12 +3,14 @@ import time
 import os
 from datetime import datetime
 
+from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
 from .server_tools import reset_database
+from .management.commands.create_session import create_pre_authenticated_session
 
 MAX_WAIT = 10
 
@@ -100,11 +102,13 @@ class FunctionalTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
 
+
     @wait
     def wait_to_be_logged_in(self, email):
         self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
+
 
     @wait
     def wait_to_be_logged_out(self, email):
@@ -112,9 +116,33 @@ class FunctionalTest(StaticLiveServerTestCase):
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
 
+
     def add_list_item(self, item_text):
         num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
         self.get_item_input_box().send_keys(item_text)
         self.get_item_input_box().send_keys(Keys.ENTER)
         item_number = num_rows + 1
         self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
+
+
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            if self.staging_port:
+                session_key = create_session_on_server(
+                                self.staging_server, email,
+                                self.staging_ssh_port,
+                                self.staging_ssh_private_key
+                            )
+            else:
+                session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+
+        ## to set a cookie we need to first  visit the domain.
+        ## 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
